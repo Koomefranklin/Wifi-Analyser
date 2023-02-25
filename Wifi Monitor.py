8826abdf74ab4e9a03dea8bab49c81952131fe2a
Frank <87543686+Koomefranklin@users.noreply.github.com>
2023-02-26 00:52:08 +0300

    Update Wifi Monitor.py

--- Wifi Monitor.py
+++ Wifi Monitor.py
@@ -1,42 +1,62 @@
 #!/usr/bin/env python
+#Koome Frank
+"""
+Simple Python program that tracks the up/downtime of wifi network
+The information is saved in a csv file for later analysis
+The libraries used are: subprocess to execute ping command and read the ssid and signal
+csv for working with csv files, datetime for time and date records, time for scheduling
+and regex for searching within the returned string from subprocess.
+"""
+
 import subprocess
 import csv
 import datetime
 import time
+import re
 
+#function to check and return the ssid and signal strength of the connected wifi
 def wifi():
     result = subprocess.check_output(['netsh', 'wlan', 'show', 'interface'])
     result = result.decode('utf-8')
     result = result.replace('\r', '')
 
-    ssid = result.split('\n')[8].split(':')[1].strip()
-    signal = result.split('\n')[18].split(':')[1].strip()
+    #regular expression(regex) is used to search within the returned string 
+    #the returned string is striped off whitespaces
+    match_name = re.search('SSID (.*?)\n', result)
+    ssid = match_name.group(1).strip(' :')
+    match_signal = re.search('Signal(.*?)%', result)
+    signal = match_signal.group(1).strip(' :')
 
     return ssid, signal
 
+#function for trying to ping and return the packet loss and latency of the network
 def ping(host):
     try:
         output = subprocess.check_output(['ping', '-n', '5', '-w', '1000', host])
         output = output.decode('utf-8')
         output = output.replace('\r', '')
 
-        latency = output.split("\n")[11].split(",")[2].split("=")[1].strip(' ms')
-        Packet_loss = output.split("\n")[9].split("(")[1].strip(' loss),%')
+        match_loss = re.search('\d+%\ loss', output)
+        packet_loss = int(match_loss.group().strip(' loss%'))
+        match_latency = re.search('Average = \d+', output)
+        latency = int(match_latency.group().split('=')[1].strip())
 
-        return Packet_loss, latency
+        return packet_loss, latency
     except subprocess.CalledProcessError:
         pass
 
+#define column names, create the csv file if it does not exist and add the first row
 header = ["Network","Date","Time","Signal(%)","Status","Packet Loss(%)","Latency(ms)"]
-
+filename = "wifi_statistics.csv"
 try:
-    with open("wifi_statistics.csv", "r") as file:
+    with open(filename, "r") as file:
         read = csv.reader(file)
 except FileNotFoundError:
-    with open("wifi_statistics.csv", "w") as newfile:
+    with open(filename, "w") as newfile:
         csvwriter = csv.writer(newfile) 
         csvwriter.writerow(header)
 
+#run the script adding  the values to the csv at intervals of 100 seconds
 while True:
     try:
         ssid, signal = wifi()
@@ -44,13 +64,15 @@ while True:
         Date = datetime.date.today()
         Time = datetime.datetime.now().time().strftime("%H: %M: %S")
 
-        with open("wifi_statistics.csv","a") as statsfile:
+        with open(filename,"a") as statsfile:
             csvwriter = csv.writer(statsfile)
             csvwriter.writerow([ssid, Date, Time, signal, "up", packet_loss, latency])
     except TypeError:
-        with open("wifi_statistics.csv","a") as statsfile:
+        with open(filename,"a") as statsfile:
             csvwriter = csv.writer(statsfile)
             csvwriter.writerow([ssid, Date, Time, signal, "down"])
-    except ValueError:
-        pass
-    time.sleep(300)
+    except AttributeError:
+        with open(filename,"a") as statsfile:
+            csvwriter = csv.writer(statsfile)
+            csvwriter.writerow("Disconnected")
+    time.sleep(100)
